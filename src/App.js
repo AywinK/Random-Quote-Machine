b22d9c5fa95c338cda3869e9853601570497c11a
AywinK <115905373+AywinK@users.noreply.github.com>
2023-08-02 00:11:06 +0100

    adds onclick logic and render logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [data, setData] = useState({});
+  const [randomSeed, setRandomSeed] = useState(Math.random());
+
+  useEffect(() => {
+    fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+      .then(res => res.json())
+      .then((data) => {
+        console.log("fetch");
+        console.log(data.quotes[Math.floor(data.quotes.length * randomSeed)]);
+        setData(data.quotes[Math.floor(data.quotes.length * randomSeed)]);
+      });
+  }, [randomSeed]);
+
+  const handleClick = () => {
+    setRandomSeed(Math.random());
+  }
+
+
   return (
     <main id="wrapper">
       <section id="quote-box">
-        <q id="text">quote here quote herequote herequote herequote herequote herequote herequote herequote here</q>
-        <cite id="author">author here</cite>
-        <button id="new-quote">New Quote</button>
+        <q id="text">{data.quote}</q>
+        <cite id="author">{data.author}</cite>
+        <button id="new-quote" onClick={handleClick}>New Quote</button>
         <a id="tweet-quote" href="twitter.com/intent/tweet"><TwitterIcon fontSize="large" /></a>
       </section>
     </main>
