2cfd751643bc3c36d9bc658dd4ce0f031010adbe
AywinK <115905373+AywinK@users.noreply.github.com>
2023-08-01 23:27:04 +0100

    adds styling to app.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function App() {
   return (
-    <main>
-
+    <main id="wrapper">
+      <section id="quote-box">
+        <q id="text">quote here quote herequote herequote herequote herequote herequote herequote herequote here</q>
+        <cite id="author">author here</cite>
+        <button id="new-quote">New Quote</button>
+        <a id="tweet-quote" href="twitter.com/intent/tweet"><TwitterIcon fontSize="large" /></a>
+      </section>
     </main>
   );
 }
